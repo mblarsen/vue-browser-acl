ba13592d253a8fdcd56cfd57292f0fbbd7c044bb
Michael Bøcker-Larsen <m19n@pm.me>
2019-08-15 15:44:00 +0800

    refactor(rollup): simplify builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,41 +3,38 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 
-export default [
-  {
-    input: './index.js',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
-        exports: 'named'
-      }, {
-        file: pkg.module,
-        sourcemap: true,
-        format: 'esm'
-      }
-    ],
-    plugins: [
-      resolve(),
-      terser()
-    ]
-  },
-  {
-    input: './index.js',
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      sourcemap: true,
-      name: 'VueBrowserAcl',
-      exports: 'named'
-    },
-    plugins: [
-      resolve(),
-      babel({
-        exclude: 'node_modules/**'
-      }),
-      terser()
-    ]
+const targetsMap = {
+  [pkg.module]: 'esm',
+  [pkg.main]: 'cjs',
+  [pkg.browser]: 'umd',
+}
+
+const output = (name = null) => target => {
+  const output = {
+    file: target,
+    format: targetsMap[target],
+    sourcemap: true,
+    exports: 'named',
   }
+  if (name) {
+    output.name = name
+  }
+  return output
+}
+
+const config = (targets, name = null, plugins = []) => ({
+  input: './index.js',
+  output: targets.map(output(name)),
+  plugins: [
+    resolve(),
+    ...plugins,
+    terser(),
+  ]
+})
+
+export default [
+  config([pkg.main, pkg.module]),
+  config([pkg.browser], 'VueBrowserAcl', [
+    babel({ exclude: 'node_modules/**' }),
+  ]),
 ];
